b8b3a34a97aaa85297183efb95aa29cdf9eebbdb
codewaseem <codewaseem@gmail.com>
2022-01-11 15:12:09 +0530

    sign up form demo

--- src/components/auth/SignUp.test.tsx
+++ src/components/auth/SignUp.test.tsx
@@ -1,12 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import TestAppProvider from "testUtils/TestAppProvider";
 import { SignUp } from "./SignUp";
+import userEvent from "@testing-library/user-event";
 
 describe("Signup Flow", () => {
   let firstNameInput: HTMLInputElement | null = null;
   let lastNameInput: HTMLInputElement | null = null;
   let phoneNumberInput: HTMLInputElement | null = null;
   let passwordInput: HTMLInputElement | null = null;
+  let createAccBtn: HTMLButtonElement | null = null;
+  let createAccountButton: HTMLButtonElement | null = null;
 
   beforeEach(() => {});
   it("should have all the form elements in en and ru locale", () => {
@@ -40,5 +43,51 @@ describe("Signup Flow", () => {
     screen.getByLabelText<HTMLInputElement>(/Фамилия/i);
     screen.getByLabelText<HTMLInputElement>(/Номер телефона/i);
     screen.getByLabelText<HTMLInputElement>(/Пароль/i);
+  });
+
+  it("after filling inputs, it should ask for otp", async () => {
+    render(
+      <TestAppProvider>
+        <SignUp
+          onSignUp={async () => {
+            //
+          }}
+          onVerifyOTP={async () => true}
+        />
+      </TestAppProvider>
+    );
+
+    firstNameInput = screen.getByLabelText<HTMLInputElement>(/First Name/i);
+    lastNameInput = screen.getByLabelText<HTMLInputElement>(/Last Name/i);
+    phoneNumberInput = screen.getByLabelText<HTMLInputElement>(/Phone Number/i);
+    passwordInput = screen.getByLabelText<HTMLInputElement>(/Password/i);
+    createAccBtn = screen.getByRole<HTMLButtonElement>("button", {
+      name: /Create Account/i,
+    });
+
+    expect(createAccBtn).toBeDisabled();
+    userEvent.type(firstNameInput, "John");
+    userEvent.type(lastNameInput, "Doe");
+    userEvent.type(phoneNumberInput, "1234567890");
+    userEvent.type(passwordInput, "password");
+
+    expect(createAccBtn).toBeEnabled();
+    expect(screen.queryByLabelText("Enter OTP")).toBeNull();
+
+    userEvent.click(createAccBtn);
+
+    const otpInput = await screen.findByLabelText(/Enter OTP/i);
+    const verifyOTPBtn = screen.getByRole("button", { name: /Verify OTP/i });
+    expect(otpInput).toBeRequired();
+
+    expect(verifyOTPBtn).toBeDisabled();
+    userEvent.type(otpInput, "123456");
+    expect(verifyOTPBtn).toBeEnabled();
+
+    userEvent.click(verifyOTPBtn);
+
+    expect(
+      await screen.findByRole("button", { name: /Verify OTP/i })
+    ).not.toBeVisible();
   });
 });
--- src/components/auth/SignUp.tsx
+++ src/components/auth/SignUp.tsx
@@ -1,16 +1,163 @@
-import { Input, TextField } from "@mui/material";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  TextField,
+} from "@mui/material";
 import { Box } from "@mui/system";
 import { useTranslations } from "next-intl";
+import { ChangeEventHandler, useState } from "react";
 
-export function SignUp() {
+type FormInputs = {
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+  password: string;
+};
+
+function validateFormInputs(inputs: FormInputs): {
+  isValid: boolean;
+  errors: string[];
+} {
+  const { firstName, lastName, phoneNumber, password } = inputs;
+
+  const errors: string[] = [];
+
+  if (!firstName) {
+    errors.push("First name is required");
+  }
+
+  if (!lastName) {
+    errors.push("Last name is required");
+  }
+
+  if (!phoneNumber) {
+    errors.push("Phone number is required");
+  }
+
+  if (!password) {
+    errors.push("Password is required");
+  }
+
+  return { isValid: !errors.length, errors };
+}
+
+type EnterOTPDialogProps = {
+  isOpen?: boolean;
+  isValidOTPInput?: (otp: string) => boolean;
+  onVerify?: (otp: string) => void;
+};
+export function EnterOTPDialog({
+  isOpen = false,
+  isValidOTPInput = (otp) => !!otp,
+  onVerify = () => null,
+}: EnterOTPDialogProps) {
+  const [otp, setOTP] = useState("");
   const t = useTranslations("SignUp");
 
   return (
+    <Dialog open={isOpen}>
+      <DialogTitle>{t("verifyOTP")}</DialogTitle>
+      <DialogContent>
+        <TextField
+          autoFocus
+          margin="dense"
+          name="enterOTP"
+          label={t("enterOTP")}
+          type="tel"
+          value={otp}
+          onChange={(e) => setOTP(e.target.value)}
+          required
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button disabled={!isValidOTPInput(otp)} onClick={() => onVerify(otp)}>
+          {t("verifyOTP")}
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
+
+type SignUpProps = {
+  onSignUp?: (inputs: FormInputs) => Promise<void>;
+  onVerifyOTP?: (otp: string) => Promise<boolean>;
+};
+export function SignUp({
+  onSignUp = async () => null,
+  onVerifyOTP = async () => true,
+}: SignUpProps) {
+  const [formInputs, setFormInput] = useState<FormInputs>({
+    firstName: "",
+    lastName: "",
+    phoneNumber: "",
+    password: "",
+  });
+  const [showOTPDialog, setShowOTPDialog] = useState(false);
+  const t = useTranslations("SignUp");
+
+  const handleInputChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setFormInput({
+      ...formInputs,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const { isValid } = validateFormInputs(formInputs);
+
+  const handleVerifyOTP = async (otp: string) => {
+    const isValidOTP = await onVerifyOTP(otp);
+    if (isValidOTP) {
+      await onSignUp(formInputs);
+      setShowOTPDialog(false);
+    }
+  };
+
+  return (
     <Box>
-      <TextField required label={t("firstName")} />
-      <TextField required label={t("lastName")} />
-      <TextField required label={t("phoneNumber")} type={"tel"} />
-      <TextField required label={t("password")} type="password" />
+      <TextField
+        required
+        name="firstName"
+        label={t("firstName")}
+        value={formInputs.firstName}
+        onChange={handleInputChange}
+      />
+      <TextField
+        required
+        name="lastName"
+        label={t("lastName")}
+        value={formInputs.lastName}
+        onChange={handleInputChange}
+      />
+      <TextField
+        name="phoneNumber"
+        required
+        label={t("phoneNumber")}
+        type={"tel"}
+        value={formInputs.phoneNumber}
+        onChange={handleInputChange}
+      />
+      <TextField
+        name="password"
+        required
+        label={t("password")}
+        type="password"
+        value={formInputs.password}
+        onChange={handleInputChange}
+      />
+      <Button
+        variant="contained"
+        color="primary"
+        disabled={!isValid}
+        onClick={() => {
+          setShowOTPDialog(true);
+        }}
+      >
+        {t("createAccount")}
+      </Button>
+      <EnterOTPDialog isOpen={showOTPDialog} onVerify={handleVerifyOTP} />
     </Box>
   );
 }
